2e58e540ac01b2bfaac06c834eb7f170c708d55e
bn1654 <stikenloni@gmail.com>
2025-11-10 10:50:19 +0700

    создание шаблонов

--- Library/catalog/views.py
+++ Library/catalog/views.py
@@ -7,8 +7,8 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count() 
     return render(
-        request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+    request,
+    'index.html',
+     context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+)
     
