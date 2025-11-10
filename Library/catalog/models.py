194d5acc2cb54eebadc2623ca9922dc48fa7845e
bn1654 <stikenloni@gmail.com>
2025-11-10 09:51:31 +0700

    создание моделей автора и данных книги, миграция бд

--- Library/catalog/models.py
+++ Library/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 
 class Genre(models.Model):
@@ -22,3 +23,37 @@ class Book(models.Model):
         return reverse('book-detail', args=[str(self.id)])
 
 
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID книги уникален для всей библиотеки")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+
+    def __str__(self):
+        return '%s (%s)' % (self.id,self.book.title)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Умер', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
